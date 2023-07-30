757eba3b9ebd39594ecbf71817b4cefde8ce0db9
SurKaa <suralcarywn@qq.com>
2023-07-30 18:49:20 +0800

    Create UserDao.go
    Dao 直接对数据库操作层

---
+++ dao/UserDao.go
@@ -0,0 +1,17 @@
+package dao
+
+import "DouyinBackend/module"
+
+type UserDao interface {
+	// InsertUser 向数据库中插入用户
+	InsertUser(user module.User) error
+
+	// DeleteUserById 通过id删除用户 (将is_delete改为1)
+	DeleteUserById(id int64) error
+
+	// UpdateUserById 通过ID更新用户信息
+	UpdateUserById(id int64) error
+
+	// GetUserById 通过ID获取用户信息
+	GetUserById(id int64) (module.User, error)
+}
