b4479cf416748e0e3b921bfc51a552bccb2303b3
SurKaa <suralcarywn@qq.com>
2023-07-30 21:32:36 +0800

    Update Entity.go
    快速创建一个实体 并配备CreateAt 和UpdateAt

--- module/Entity.go
+++ module/Entity.go
@@ -1,11 +1,22 @@
 package module
 
-import "time"
+import (
+	"DouyinBackend/util"
+	"time"
+)
 
 // Entity 所有表的module都应由以下字段 并继承以下字段
 type Entity struct {
-	ID       int64     `json:"id"`                  // 主键
-	IsDelete int32     `json:"is_delete,omitempty"` // 逻辑删除
-	CreateAt time.Time `json:"create_at,omitempty"` // 创建时间
-	UpdateAt time.Time `json:"update_at,omitempty"` // 更新时间
+	ID       int64     `json:"id,omitempty" gorm:"primaryKey"`                                                            // 主键
+	IsDelete int32     `json:"is_delete,omitempty"`                                                                       // 逻辑删除
+	CreateAt time.Time `json:"create_at,omitempty" gorm:"not null;default:CURRENT_TIMESTAMP"`                             // 创建时间
+	UpdateAt time.Time `json:"update_at,omitempty" gorm:"not null;default:CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"` // 更新时间
+}
+
+// CreateEntity 快速创建一个实体
+func CreateEntity() Entity {
+	return Entity{
+		ID:       util.SF.NextID(),
+		IsDelete: 0,
+	}
 }
