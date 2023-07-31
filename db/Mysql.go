8c1bc79ee5ec825a521d96f11ecbb47c9c3380ff
SurKaa <suralcarywn@qq.com>
2023-07-31 11:53:28 +0800

    Update Mysql.go
    不返回err 直接退出程序

--- db/Mysql.go
+++ db/Mysql.go
@@ -4,14 +4,15 @@ import (
 	"DouyinBackend/config"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"log"
 )
 
 // GetMysqlConnection 获取Mysql连接
-func GetMysqlConnection() (*gorm.DB, error) {
+func GetMysqlConnection() *gorm.DB {
 	// 读取到之后转成string
 	db, err := gorm.Open(mysql.Open(config.Config.MysqlDNS), &gorm.Config{})
 	if err != nil {
-		return nil, err
+		log.Fatalf("获取数据库连接失败: %v", err)
 	}
-	return db, nil
+	return db
 }
