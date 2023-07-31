d258282364d9563c4a9dfdb8e9c4032607fdc4e2
SurKaa <suralcarywn@qq.com>
2023-07-31 11:53:47 +0800

    Update UserDaoImpl.go

--- dao/impl/UserDaoImpl.go
+++ dao/impl/UserDaoImpl.go
@@ -1,6 +1,7 @@
 package impl
 
 import (
+	"DouyinBackend/db"
 	"DouyinBackend/module"
 )
 
@@ -8,24 +9,31 @@ type UserDaoImpl struct {
 }
 
 // 要使用impl时可以通过这样调用
-// var userDaoImpl dao.UserDao = UserDaoImpl{}
+// var userDaoImpl dao.UserDao = impl.UserDaoImpl{}
 
 // InsertUser 向数据库中插入用户
 func (userDao UserDaoImpl) InsertUser(user module.User) error {
-	return nil
+	return db.GetMysqlConnection().Create(&user).Error
 }
 
-// DeleteUserById 通过id删除用户 (将is_delete改为1)
+// DeleteUserById 通过id删除用户 (将is_delete改为Deleted)
 func (userDao UserDaoImpl) DeleteUserById(id int64) error {
-	return nil
+	return db.GetMysqlConnection().Model(&module.User{}).Where("Where id = ?", id).Updates(module.User{
+		Entity: module.Entity{
+			ID:       id,
+			IsDelete: module.Deleted,
+		},
+	}).Error
 }
 
 // UpdateUserById 通过ID更新用户信息
-func (userDao UserDaoImpl) UpdateUserById(id int64) error {
-	return nil
+func (userDao UserDaoImpl) UpdateUserById(user module.User) error {
+	return db.GetMysqlConnection().Model(&module.User{}).Where("where id = ?", user.ID).Updates(user).Error
 }
 
 // GetUserById 通过ID获取用户信息
 func (userDao UserDaoImpl) GetUserById(id int64) (module.User, error) {
-	return module.User{}, nil
+	var user module.User
+	result := db.GetMysqlConnection().First(&user, id)
+	return user, result.Error
 }
