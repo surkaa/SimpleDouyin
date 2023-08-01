729ef768d133435daa6151450c8ff110ca860634
SurKaa <suralcarywn@qq.com>
2023-08-01 14:39:20 +0800

    Update UserInfoBody.go
    * ParseUserInfoBody 解析获取用户信息请求体
    * 将token的类型换成string'

--- request/UserInfoBody.go
+++ request/UserInfoBody.go
@@ -1,7 +1,23 @@
 package request
 
+import (
+	"github.com/gin-gonic/gin"
+	"strconv"
+)
+
 // UserInfoBody 获取用户信息请求体
 type UserInfoBody struct {
-	UserId int64 `json:"user_id"`
-	Token  int64 `json:"token"`
+	UserId int64  `json:"user_id"`
+	Token  string `json:"token"`
+}
+
+// ParseUserInfoBody 解析获取用户信息请求体
+func ParseUserInfoBody(c *gin.Context) (UserInfoBody, error) {
+	userId := c.Query("user_id")
+	token := c.Query("token")
+	userIdInt, _ := strconv.ParseInt(userId, 10, 64)
+	return UserInfoBody{
+		UserId: userIdInt,
+		Token:  token,
+	}, nil
 }
