ab0ee8f495931e692a98d75f413d0f471e0e486e
SurKaa <suralcarywn@qq.com>
2023-08-01 10:55:45 +0800

    Update UserLoginBody.go
    ParseUserLoginBody 解析用户注册请求体

--- request/UserLoginBody.go
+++ request/UserLoginBody.go
@@ -1,7 +1,19 @@
 package request
 
+import "github.com/gin-gonic/gin"
+
 // UserLoginBody 用户注册请求体
 type UserLoginBody struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
+}
+
+// ParseUserLoginBody 解析用户注册请求体
+func ParseUserLoginBody(c *gin.Context) (UserLoginBody, error) {
+	username := c.Query("username")
+	password := c.Query("password")
+	return UserLoginBody{
+		Username: username,
+		Password: password,
+	}, nil
 }
