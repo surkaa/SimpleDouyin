156921b7d97c8d0357b7808badf7e6a08a84995a
SurKaa <suralcarywn@qq.com>
2023-08-01 12:33:06 +0800

    Create Redis.go
    GetRedisConnection 获取redis连接

---
+++ db/Redis.go
@@ -0,0 +1,16 @@
+package db
+
+import (
+	"DouyinBackend/config"
+	"github.com/go-redis/redis/v8"
+)
+
+// GetRedisConnection 获取redis连接
+func GetRedisConnection() *redis.Client {
+	return redis.NewClient(&redis.Options{
+		Addr:     config.Config.Redis.Addr,
+		Password: config.Config.Redis.Password,
+		DB:       config.Config.Redis.DB,
+		PoolSize: config.Config.Redis.PoolSize,
+	})
+}
