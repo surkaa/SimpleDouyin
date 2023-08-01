c821c74cee051fd42c1917ec0a1937a832daff22
SurKaa <suralcarywn@qq.com>
2023-08-01 10:44:13 +0800

    Update UserRegisterBody.go
    Parse 解析请求体中的参数得到注册请求体

--- request/UserRegisterBody.go
+++ request/UserRegisterBody.go
@@ -1,8 +1,20 @@
 package request
 
+import "github.com/gin-gonic/gin"
+
 // UserRegisterBody 用户注册请求体
 type UserRegisterBody struct {
-	Username      string `json:"username"`
-	Password      string `json:"password"`
-	CheckPassword string `json:"checkPassword"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	// CheckPassword string `json:"checkPassword"` TODO 未来可试
+}
+
+// ParseUserRegisterBody 解析请求体中的参数得到注册请求体
+func ParseUserRegisterBody(c *gin.Context) (UserRegisterBody, error) {
+	username := c.Query("username")
+	password := c.Query("password")
+	return UserRegisterBody{
+		Username: username,
+		Password: password,
+	}, nil
 }
