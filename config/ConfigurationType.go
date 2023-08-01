8f8f627688fb8360a323c58ba4b0a6da457373e9
SurKaa <suralcarywn@qq.com>
2023-08-01 12:32:34 +0800

    Update ConfigurationType.go
    Redis Redis配置

--- config/ConfigurationType.go
+++ config/ConfigurationType.go
@@ -3,4 +3,13 @@ package config
 type Configuration struct {
 	MysqlDNS string `json:"mysqlDNS"`
 	WorkerID int64  `json:"workerID"`
+	Redis    Redis  `json:"redis"`
+}
+
+// Redis Redis配置
+type Redis struct {
+	Addr     string `json:"Addr"`     // 链接地址及其端口
+	Password string `json:"Password"` // no password set
+	DB       int    `json:"DB"`       // use default DB
+	PoolSize int    `json:"PoolSize"` // 连接池大小
 }
