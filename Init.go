eab77361e11aa635a7df009c71f9d2ac21b9626e
SurKaa <suralcarywn@qq.com>
2023-08-03 15:34:37 +0800

    引入redis logrus

--- Init.go
+++ Init.go
@@ -5,10 +5,12 @@ import (
 	"DouyinBackend/router"
 	"DouyinBackend/util"
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 )
 
 func initApp(r *gin.Engine) {
 	config.LoadConfig("./configuration.json")
 	util.SF = util.NewSnowflake(config.Config.WorkerID)
 	router.RegisterRouter(r)
+	logrus.SetLevel(logrus.DebugLevel)
 }
