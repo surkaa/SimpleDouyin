094f4390a9945e4bb9ab392e83a55d4a7ae302a1
SurKaa <suralcarywn@qq.com>
2023-08-16 22:17:38 +0800

    移动main方法

--- backend-server/Init.go
+++ backend-server/Init.go
@@ -5,7 +5,16 @@ import (
 	"DouyinBackend/router"
 	"DouyinBackend/util"
 	"github.com/gin-gonic/gin"
+	"log"
 )
+
+func startServer() {
+	g := gin.Default()
+	initApp(g)
+	if err := g.Run(); err != nil {
+		log.Fatalf("启动失败")
+	}
+}
 
 func initApp(r *gin.Engine) {
 	config.LoadConfig("../backend-server-configuration.json")
